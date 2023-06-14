0b93d1be34e89ed70de45b395c92305ea94ab20c
Kaan Tosun <tr.kaantsn@gmail.com>
2023-06-14 21:40:54 +0300

    first commit

---
+++ example.js
@@ -0,0 +1,16 @@
+
+
+// Function to calculate the area of a rectangle
+function calculateRectangleArea(length, width) {
+  // Multiply the length and width to get the area
+  var area = length * width;
+  
+  // Return the calculated area
+  return area;
+}
+
+// Call the function and store the result in a variable
+var rectangleArea = calculateRectangleArea(10, 5);
+
+// Print out the result
+console.log("The area of the rectangle is: " + rectangleArea);
